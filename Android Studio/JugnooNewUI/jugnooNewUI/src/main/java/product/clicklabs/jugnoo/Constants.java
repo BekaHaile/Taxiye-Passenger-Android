3d7ba5b6e09a737b45bfb0a084e1ea50c92250d5
Shankar <shankar@jugnoo.in>
2015-12-11 16:34:07 +0530

    feature_notification_call_driver: added call driver push handling

---
+++ Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/Constants.java
@@ -0,0 +1,15 @@
+package product.clicklabs.jugnoo;
+
+/**
+ * Created by socomo20 on 12/11/15.
+ */
+public interface Constants {
+
+	String KEY_FLAG = "flag";
+	String KEY_MESSAGE = "message";
+
+	String KEY_ENGAGEMENT_ID = "engagement_id";
+	String KEY_PHONE_NO = "phone_no";
+	String KEY_DRIVER_CAR_NO = "driver_car_no";
+
+}
