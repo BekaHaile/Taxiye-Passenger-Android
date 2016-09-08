c4d8f693f42fc4db6d47bc92a3509275f44b5bcd
Shankar <shankar@jugnoo.in>
2016-09-08 11:26:15 +0530

    feature_freecharge: dev creds

--- Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/retrofit/RestClient.java
+++ Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/retrofit/RestClient.java
@@ -151,7 +151,7 @@ public class RestClient {
             RestAdapter.Builder builder = new RestAdapter.Builder()
                     .setEndpoint(Config.getFreshServerUrl())
                     .setClient(new Ok3Client(getOkHttpClient()))
-                    .setLog(fooLog)
+//                    .setLog(fooLog)
                     .setLogLevel(RestAdapter.LogLevel.FULL);
 
             RestAdapter restAdapter = builder.build();
