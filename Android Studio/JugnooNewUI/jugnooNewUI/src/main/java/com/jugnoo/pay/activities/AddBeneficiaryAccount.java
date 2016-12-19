142074003bc36813f0f0d8f645860fc1a6d5a5a8
Keval <keval.langalia@jugnoo.in>
2016-12-19 13:05:42 +0530

    started working on "add beneficiary account" UI completed

---
+++ Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/com/jugnoo/pay/activities/AddBeneficiaryAccount.java
@@ -0,0 +1,15 @@
+package com.jugnoo.pay.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import product.clicklabs.jugnoo.R;
+
+public class AddBeneficiaryAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_beneficiary_account);
+    }
+}
