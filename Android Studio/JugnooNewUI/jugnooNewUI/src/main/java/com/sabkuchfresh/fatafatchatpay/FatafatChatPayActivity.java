ec3efeefa76db8aadd86609ca55f63726fac61ae
Gaurav Choudhary <gaurav.chaudhary@mail.click-labs.com>
2017-12-26 11:34:09 +0530

    Show button text ok on already paid dialog

--- Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/com/sabkuchfresh/fatafatchatpay/FatafatChatPayActivity.java
+++ Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/com/sabkuchfresh/fatafatchatpay/FatafatChatPayActivity.java
@@ -155,7 +155,7 @@ public class FatafatChatPayActivity extends RazorpayBaseActivity implements View
     private void showAlreadyPaidAlert(String message) {
 
         DialogPopup.alertPopupWithListener(this, "", getResources().getString(R.string.txt_fatafat_order_already_paid),
-                message, new View.OnClickListener() {
+                getString(android.R.string.ok), new View.OnClickListener() {
                     @Override
                     public void onClick(final View v) {
                         // exit
