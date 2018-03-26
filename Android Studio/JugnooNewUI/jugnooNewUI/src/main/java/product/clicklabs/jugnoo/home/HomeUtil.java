c008e5233b2b711f203b406dc363a8976f37441e
Shankar <shankar@jugnoo.in>
2018-03-26 11:12:10 +0530

    feat_operator_div: operator id checks added in coupons promotions

--- Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/home/HomeUtil.java
+++ Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/home/HomeUtil.java
@@ -181,18 +181,21 @@ public class HomeUtil {
 		params.put(Constants.KEY_APP_VERSION, String.valueOf(MyApplication.getInstance().appVersion()));
 		params.put(Constants.KEY_DEVICE_TYPE, Data.DEVICE_TYPE);
 		params.put(Constants.KEY_LOGIN_TYPE, String.valueOf(0));
+		params.put(Constants.KEY_CUSTOMER_PACKAGE_NAME, MyApplication.getInstance().getPackageName());
 	}
 
 	public static void addDefaultParams(Map<String, String> params){
 		params.put(Constants.KEY_APP_VERSION, String.valueOf(MyApplication.getInstance().appVersion()));
 		params.put(Constants.KEY_DEVICE_TYPE, Data.DEVICE_TYPE);
 		params.put(Constants.KEY_LOGIN_TYPE, String.valueOf(0));
+		params.put(Constants.KEY_CUSTOMER_PACKAGE_NAME, MyApplication.getInstance().getPackageName());
 	}
 
 	public void putDefaultParamsMultipart(MultipartTypedOutput multipartTypedOutput){
 		multipartTypedOutput.addPart(Constants.KEY_APP_VERSION, new TypedString(String.valueOf(MyApplication.getInstance().appVersion())));
 		multipartTypedOutput.addPart(Constants.KEY_DEVICE_TYPE, new TypedString(String.valueOf(Data.DEVICE_TYPE)));
 		multipartTypedOutput.addPart(Constants.KEY_LOGIN_TYPE, new TypedString(String.valueOf(0)));
+		multipartTypedOutput.addPart(Constants.KEY_CUSTOMER_PACKAGE_NAME, new TypedString(MyApplication.getInstance().getPackageName()));
 	}
 
 	public ArrayList<SearchResult> getSavedPlacesWithHomeWork(Activity activity){
