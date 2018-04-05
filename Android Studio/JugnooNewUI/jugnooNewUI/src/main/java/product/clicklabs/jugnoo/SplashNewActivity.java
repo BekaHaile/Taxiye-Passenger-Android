65fc8f5626bbbded059597ccca3187760dfd9318
Shankar <shankar@jugnoo.in>
2018-04-05 11:02:16 +0530

    feat_rtl: final fixes

--- Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/SplashNewActivity.java
+++ Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/SplashNewActivity.java
@@ -45,7 +45,6 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -2598,7 +2597,7 @@ public class SplashNewActivity extends BaseActivity implements  Constants, GAAct
 			dialog.getWindow().getAttributes().windowAnimations = R.style.Animations_LoadingDialogFade;
 			dialog.setContentView(R.layout.dialog_edittext_confirm);
 
-			FrameLayout frameLayout = (FrameLayout) dialog.findViewById(R.id.rv);
+			RelativeLayout frameLayout = (RelativeLayout) dialog.findViewById(R.id.rv);
 			new ASSL(activity, frameLayout, 1134, 720, true);
 
 			WindowManager.LayoutParams layoutParams = dialog.getWindow().getAttributes();
