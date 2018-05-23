00e9add0077f66566c67d9b78e5e5d4e60ada2f8
Parminder <parminder.saini@jungleworks.co>
2018-05-23 17:44:44 +0530

    stripe_integration: Card integration with checks done

--- Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/stripe/StripeAddCardFragment.java
+++ Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/stripe/StripeAddCardFragment.java
@@ -1,9 +1,9 @@
 package product.clicklabs.jugnoo.stripe;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +14,9 @@ import com.stripe.android.Stripe;
 import com.stripe.android.TokenCallback;
 import com.stripe.android.model.Card;
 import com.stripe.android.model.Token;
-import com.stripe.android.view.CardInputWidget;
+import com.stripe.android.view.CardNumberEditText;
+import com.stripe.android.view.ExpiryDateEditText;
+import com.stripe.android.view.StripeEditText;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -29,20 +31,48 @@ import product.clicklabs.jugnoo.utils.Log;
 public class StripeAddCardFragment extends Fragment {
 
 
-    @Bind(R.id.card_input_widget)
-    CardInputWidget cardInputWidget;
     @Bind(R.id.btn_add_card)
     Button btnAddCard;
     Stripe stripe;
 
 
     private static final String TAG = StripeAddCardFragment.class.getName();
+    @Bind(R.id.edt_card_number)
+    CardNumberEditText edtCardNumber;
+    @Bind(R.id.edt_date)
+    ExpiryDateEditText edtDate;
+    @Bind(R.id.edt_cvv)
+    StripeEditText edtCvv;
+
+    private static final String ARGS_EDIT_MODE = "edit_mode";
+    private boolean editMode = false;
+    public StripeAddCardFragment newInstance(Bundle bundle){
+        StripeAddCardFragment stripeAddCardFragment = new StripeAddCardFragment();
+        stripeAddCardFragment.setArguments(bundle);
+        return stripeAddCardFragment;
+
+
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        editMode = getArguments()!=null && getArguments().containsKey(ARGS_EDIT_MODE);
+
+
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_add_card, container, false);
         stripe = new Stripe(getActivity(), BuildConfig.STRIPE_KEY);
         ButterKnife.bind(this, rootView);
+        edtCardNumber.setErrorColor(ContextCompat.getColor(getActivity(),R.color.red_status));
+        edtDate.setErrorColor(ContextCompat.getColor(getActivity(),R.color.red_status));
+        edtCvv.setErrorColor(ContextCompat.getColor(getActivity(),R.color.red_status));
         return rootView;
     }
 
@@ -55,24 +85,39 @@ public class StripeAddCardFragment extends Fragment {
 
     @OnClick(R.id.btn_add_card)
     public void onViewClicked() {
-        Card cardToSave = cardInputWidget.getCard();
-        if (cardToSave == null) {
-            Toast.makeText(getActivity(), "Invalid Card Data", Toast.LENGTH_SHORT).show();
+        String cardNumber = edtCardNumber.getCardNumber();
+        int[] cardDate = edtDate.getValidDateFields();
+        if (cardNumber == null || cardDate == null || cardDate.length != 2) {
+            return ;
+        }
+
+
+
+        Card card = new Card(
+                edtCardNumber.getCardNumber(),
+                cardDate[0],
+                cardDate[1],
+                edtCvv.getText().toString()
+        );
+
+
+        if(!card.validateCard()){
             return;
         }
 
         stripe.createToken(
-                cardToSave,
+                card,
                 new TokenCallback() {
                     public void onSuccess(Token token) {
                         // Send token to your server
 
-                        Log.i(TAG,token.getId());
-                        Log.i(TAG,token.toString());
+                        Log.i(TAG, token.getId());
+                        Log.i(TAG, token.toString());
                     }
+
                     public void onError(Exception error) {
                         // Show localized error message
-                        Log.e(TAG,error.getMessage());
+                        Log.e(TAG, error.getMessage());
                         Toast.makeText(getContext(),
                                 error.getLocalizedMessage(),
                                 Toast.LENGTH_LONG
@@ -83,4 +128,6 @@ public class StripeAddCardFragment extends Fragment {
 
 
     }
+
+
 }
