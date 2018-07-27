ccbae8c5fbced623f94884d34bb832d78bf816d7
Parminder <parminder.saini@jungleworks.co>
2018-07-27 19:06:50 +0530

    feat_target_version: Unique Imei Id sorted

--- Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/utils/UniqueIMEIID.java
+++ Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/utils/UniqueIMEIID.java
@@ -1,28 +1,58 @@
 package product.clicklabs.jugnoo.utils;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+
+import product.clicklabs.jugnoo.BuildConfig;
+import product.clicklabs.jugnoo.permission.PermissionCommon;
 
 public class UniqueIMEIID {
 
+
 	public static String getUniqueIMEIId(Context context) {
-		try {
-			String imei;
+
+		String imei = null,meid= null,serial = null,androidSecureId = null;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 			if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
 				TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-				imei = telephonyManager.getDeviceId();
-			} else {
-				imei = Build.SERIAL;
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+					if (telephonyManager != null) {
+						imei = telephonyManager.getImei();
+						meid = telephonyManager.getMeid();
+					}
+					serial = Build.getSerial();
+				} else {
+					imei = telephonyManager.getDeviceId();
+					serial = Build.SERIAL;
+
+				}
+
 			}
-			return imei;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return Build.SERIAL;
+		} else {
+			TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+			if(telephonyManager!=null)imei = telephonyManager.getDeviceId();
+			serial = Build.SERIAL;
 		}
+
+
+		androidSecureId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+		if(!TextUtils.isEmpty(imei))return imei;
+//		if(!TextUtils.isEmpty(meid))return meid;
+		if(!TextUtils.isEmpty(serial))return serial;
+
+		return androidSecureId;
+
+
+
+
 	}
-	
+
 }
