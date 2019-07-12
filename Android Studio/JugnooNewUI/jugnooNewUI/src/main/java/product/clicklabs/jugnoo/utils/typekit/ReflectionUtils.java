4a91edc20035a21e13975e1c2550caf3bbc8b5a8
Amandeep Kamboj <amandeep.kamboj@aman.local>
2019-07-12 17:10:07 +0530

    Q crash fixed

--- Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/utils/typekit/ReflectionUtils.java
+++ Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/product/clicklabs/jugnoo/utils/typekit/ReflectionUtils.java
@@ -22,7 +22,9 @@ class ReflectionUtils {
 
     static Object getValue(Field field, Object obj) {
         try {
-            return field.get(obj);
+            if(field!=null && obj!=null && field.get(obj)!=null) {
+                return field.get(obj);
+            }
         } catch (IllegalAccessException ignored) {
         }
         return null;
@@ -30,6 +32,7 @@ class ReflectionUtils {
 
     static void setValue(Field field, Object obj, Object value) {
         try {
+            if(field!=null && obj!=null && field.get(obj)!=null)
             field.set(obj, value);
         } catch (IllegalAccessException ignored) {
         }
