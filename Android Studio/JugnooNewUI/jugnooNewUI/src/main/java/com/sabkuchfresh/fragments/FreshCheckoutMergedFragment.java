95f1195542792f3e56fc435d469de35a4127cefa
Robin <robin.malhotra@junglworks.com>
2020-01-22 16:47:50 +0530

    From:> wl_luckyu: vehicle list in menus visibility depends on key on values-wl.xml

--- Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/com/sabkuchfresh/fragments/FreshCheckoutMergedFragment.java
+++ Android Studio/JugnooNewUI/jugnooNewUI/src/main/java/com/sabkuchfresh/fragments/FreshCheckoutMergedFragment.java
@@ -826,7 +826,7 @@ public class FreshCheckoutMergedFragment extends Fragment implements GAAction, D
                 }
             }
 
-            if(vehicleInfoList.size()>1) {
+            if(vehicleInfoList.size()>1 && getResources().getBoolean(R.bool.vehicle_list_menus)) {
                 linearLayoutDeliveryVehicles.setVisibility(View.VISIBLE);
                 rvVehicles.setVisibility(View.VISIBLE);
                 textViewChooseVehicle.setVisibility(View.VISIBLE);
